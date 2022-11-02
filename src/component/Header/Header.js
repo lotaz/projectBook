0f57fc408317857d921fea030477bef978fa3c5e
lota <tidihoc123@gmail.com>
2022-11-02 10:05:50 +0700

    update

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -2,43 +2,43 @@ import React from 'react'
 import style from '../../edit/header/header.module.scss'
 import news from '../../img/Payday_Main Banner_Header_1263x60.jpg'
 import logo from '../../img/fahasa_logo.png'
-import { BsCaretDownSquare } from "react-icons/bs";
 import 'antd/dist/antd.css';
-import { InfoCircleOutlined, UserOutlined } from '@ant-design/icons';
-import { Input, Tooltip } from 'antd';
+import ListIcon from '@mui/icons-material/List';
+import PermIdentityOutlinedIcon from '@mui/icons-material/PermIdentityOutlined';
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
 export default function Header() {
   return (
     <div>
-        <div className={style.header}>
-            <img  src={news}/>
-        </div>
-        <div className={style.logo}>
-          <img src={logo}/>
-          <BsCaretDownSquare />
-          <input placeholder='Tìm kiếm sản phẩm mong muốn...'/>
-          <button className={style.button}>aaaaa</button>
-          <div>
-          <Input
-      placeholder="Enter your username"
-      prefix={<UserOutlined className="site-form-item-icon" />}
-      suffix={
-        <Tooltip title="Extra information">
-          <InfoCircleOutlined
-            style={{
-              color: 'rgba(0,0,0,.45)',
-            }}
-          />
-        </Tooltip>
-      }
-    />
-    <br />
-    <br />
-    <Input prefix="￥" suffix="RMB" />
-    <br />
-    <br />
-    <Input prefix="￥" suffix="RMB" disabled />
+      <div className={style.header}>
+        <img src={news} />
+      </div>
+      <div className={style.logo}>
+        <img src={logo} />
+        <ListIcon />
+        <input placeholder='Tìm kiếm sản phẩm mong muốn...' />
+        <button className={style.button}><SearchIcon /></button>
+
+
+
+        <div className={style.list}>
+          <div className={style.notification}>
+            <NotificationsNoneOutlinedIcon />
+            <p>Thông Báo</p>
+          </div>
+          <div className={style.cart}>
+            <ShoppingCartOutlinedIcon />
+            <p>Giỏ Hàng</p>
+          </div>
+          <div className={style.account}>
+            <PermIdentityOutlinedIcon />
+            <p>Tài Khoản</p>
           </div>
         </div>
-    </div>  
+
+
+      </div>
+    </div>
   )
 }
